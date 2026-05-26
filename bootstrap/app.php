e111415298ce61d9d7548ff478c87b0203321dcf
LHGK Admin <admin@lhgk.com>
2026-05-26 14:50:04 +0800

    tes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            TrackVisitor::class,
-        ]);
-    })
+    // ->withMiddleware(function (Middleware $middleware): void {
+    //     $middleware->web(append: [
+    //         TrackVisitor::class,
+    //     ]);
+    // })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
